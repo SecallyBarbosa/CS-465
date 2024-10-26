2570509eac97a1133fdd9509e6de53e4e0242167
SecallyBarbosa <secallybarbosa@gmail.com>
2024-10-26 04:41:52 -0400

    Committing changes before resolving conflicts

--- app_admin/src/app/services/trip-data.service.ts
+++ app_admin/src/app/services/trip-data.service.ts
@@ -1,31 +1,60 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
 import { Trip } from '../models/trip';
+import { AuthResponse } from '../models/authresponse';
+import { User } from '../models/user'; // Make sure to import User
+import { BROWSER_STORAGE } from '../storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TripDataService {
+  private url = 'http://localhost:3000/api/trips';
+  private apiBaseUrl = 'http://localhost:3000/api'; // Base URL for auth
 
   constructor(private http: HttpClient) { }
-  url = 'http://localhost:3000/api/trips';
 
-  getTrips() : Observable<Trip[]> {
+  // Fetch all trips
+  getTrips(): Observable<Trip[]> {
     return this.http.get<Trip[]>(this.url);
-    }
-  addTrip(formData: Trip) : Observable<Trip> {
-  return this.http.post<Trip>(this.url, formData);
   }
 
-  getTrip(tripCode: string) : Observable<Trip> {
-    //console.log('Inside TripDataService::getTrips');
-return this.http.get<Trip>(this.url + '/' + tripCode);
+  // Add a new trip
+  addTrip(formData: Trip): Observable<Trip> {
+    return this.http.post<Trip>(this.url, formData);
   }
-updateTrip(formData: Trip) : Observable<Trip> {
-  //conosole.log('Inside TripDataService::addTrips');
-  return this.http.put<Trip>(this.url + '/' + formData.code, formData);
 
+  // Get a trip by code
+  getTrip(tripCode: string): Observable<Trip> {
+    return this.http.get<Trip>(`${this.url}/${tripCode}`);
+  }
+
+  // Update an existing trip
+  updateTrip(formData: Trip): Observable<Trip> {
+    return this.http.put<Trip>(`${this.url}/${formData.code}`, formData);
+  }
+
+  // Login method
+  public login(user: User): Promise<AuthResponse> {
+    return this.makeAuthApiCall('login', user);
+  }
+
+  // Register method
+  public register(user: User): Promise<AuthResponse> {
+    return this.makeAuthApiCall('register', user);
+  }
+
+  // Generic method for making auth API calls
+  private makeAuthApiCall(urlPath: string, user: User): Promise<AuthResponse> {
+    const url = `${this.apiBaseUrl}/${urlPath}`;
+    return this.http.post<AuthResponse>(url, user).toPromise()
+      .catch(this.handleError);
+  }
+
+  // Handle error for HTTP requests
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
   }
 }
